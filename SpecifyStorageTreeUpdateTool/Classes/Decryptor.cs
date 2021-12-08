380b5833441d5ccc2215e9538b6b9ab313ba43c8
Warren Brown <warrenbrown@ufl.edu>
2021-12-08 13:31:47 -0500

    adding classes that were accidentally omited from last commit

---
+++ SpecifyStorageTreeUpdateTool/Classes/Decryptor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace SpecifyStorageTreeUpdateTool
+{
+    internal class Decryptor
+    {
+        private readonly int ITERATION_COUNT = 1000;
+        private readonly int SALT_LENGTH = 8;
+
+
+        private byte[] reverseHexStr(string str)
+        {
+            int len = str.Length / 2;
+            byte[] bytes = new byte[len];
+            int inx = 0;
+            for (int i = 0; i < len; i++)
+            {
+                int iVal = Convert.ToInt32(str.Substring(inx, 2), 16);
+                bytes[i] = (byte)(iVal > 127 ? iVal - 256 : iVal);
+                inx += 2;
+            }
+            return bytes;
+        }
+        
+        public string decrypt(string encryptedStr, string password)
+        {
+            byte[] input = reverseHexStr(encryptedStr);
+            byte[] salt = new byte[SALT_LENGTH];
+            System.Array.Copy(input, 0, salt, 0, SALT_LENGTH);
+            byte[] inputBytes = new byte[input.Length - salt.Length];
+            System.Array.Copy(input, salt.Length, inputBytes, 0, inputBytes.Length);
+            PKCSKeyGenerator crypto = new PKCSKeyGenerator(password, salt, ITERATION_COUNT, 1);
+            ICryptoTransform cryptoTransform = crypto.Decryptor;
+            var clearBytes = cryptoTransform.TransformFinalBlock(input, SALT_LENGTH, input.Length - SALT_LENGTH);
+            return Encoding.UTF8.GetString(clearBytes);           
+        }
+    }
+}
---
+++ SpecifyStorageTreeUpdateTool/Classes/SpecifyTools.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace SpecifyStorageTreeUpdateTool
+{
+    public class SpecifyTools
+    {
+        MySql.Data.MySqlClient.MySqlConnection conn;
+        private bool isConnected;
+        private int agentID;
+        private string userName;
+        private string database;
+        private string server;
+
+        public string AgentName {  get { return userName; } }
+        public string Database { get { return database; } }
+        public string Server { get { return server; } }
+
+        public SpecifyTools()
+        {
+
+        }
+
+        public SpecifyTools(string dbServer, string dbName, string userName, string userPassword, string userKey)
+        {
+            Decryptor decryptor = new Decryptor();
+            string[] master = decryptor.decrypt(userKey, userPassword).Split(',');
+            string connectionString = "server=" + dbServer + ";uid=" + master[0] + ";pwd=" + master[1] + ";database=" + dbName;
+            try
+            {
+                conn = new MySqlConnection(connectionString);
+                conn.Open();
+                int agentID = getAgentID(getSpecifyUserID(userName, userPassword));
+                if (agentID != -1)
+                {
+                    this.agentID = agentID;
+                    this.userName = userName;
+                    this.database = dbName;
+                    this.server = dbServer;
+                    isConnected = true;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                isConnected = false;
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public bool IsConnected()
+        {
+            return isConnected;
+        }
+
+        private int getSpecifyUserID(string username, string password)
+        {
+            try
+            {
+                string sql = "SELECT SpecifyUserID, Password FROM specifyuser WHERE Name = @Name";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@Name", username);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Decryptor decryptor = new Decryptor();
+                        if (password.Equals(decryptor.decrypt(reader.GetString(1), password)))
+                        {
+                            return reader.GetInt32(0);
+                        }
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return -1;
+        }
+
+        private int getAgentID(int specifyUserID)
+        {
+            try
+            {
+                string sql = "SELECT AgentID FROM agent WHERE SpecifyUserID = @id";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", specifyUserID);
+                object result = cmd.ExecuteScalar();
+                if (result == null)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return Convert.ToInt32(result);
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return -1;
+        }
+
+        public bool IsValidStorageID(int id)
+        {
+            if (isConnected)
+            {
+                try
+                {
+                    string sql = "SELECT count(StorageID) FROM storage WHERE StorageID = @id";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    object result = cmd.ExecuteScalar();
+                    if (result == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        int r = Convert.ToInt32(result);
+                        if (r == 1) { return true; }
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            return false;
+        }
+
+        public bool IsValidPrepGUID(string GUID)
+        {
+            if (isConnected)
+            {
+                try
+                {
+                    string sql = "SELECT count(GUID) FROM preparation WHERE GUID = @GUID";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@GUID", GUID);
+                    object result = cmd.ExecuteScalar();
+                    if (result == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        int r = Convert.ToInt32(result);
+                        if (r == 1) { return true; }
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            return false;
+        }
+
+        public bool UpdatePreparationStorageID(string prepGUID, int storageId)
+        {
+            if (isConnected)
+            {
+                try
+                {
+                    string sql = "UPDATE preparation SET StorageID = @storageId WHERE GUID = @prepGUID";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@storageId", storageId);
+                    cmd.Parameters.AddWithValue("@prepGUID", prepGUID);
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            return false;
+        }
+    }
+}
